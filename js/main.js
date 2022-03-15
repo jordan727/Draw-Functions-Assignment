c5833667f76a95f09b06fcd9e5b994d93f7e0995
jordan727 <imjordanantonio@gmail.com>
2022-03-15 09:34:41 -0600

    Init

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// Draw Functions
+
+let cnv = document.getElementById("my-canvas");
+let ctx = cnv.getContext("2d");
+cnv.width = 800;
+cnv.height = 600;
+
+function drawSolidBall(x, y, color, number) {
+    fill(color)
+    circle(x, y, 40, "fill");
+    fill("white");
+    circle(x, y, 25, "fill");
+    fill("black");
+    font("45px Arial");
+    text(number, x - 15, y + 15, "fill");
+}
+
+function drawFish(x, y, size, color) {
+    fill(color);
+    triangle(x + 5, y + 0, x + 5, y + size, x + 50, y + size / 2, "fill");
+    ellipse(x + 80, y + size / 2, 40, size / 3, 0, "fill");
+    fill("white");
+    circle(x + 100, y + size / 2, size / 10, "fill");
+    fill("black");
+    circle(x + 101, y + size / 2, size / 20, "fill");
+}
+
+drawSolidBall(500, 100, "black", "8");
+drawSolidBall(700, 20, "blue", "2");
+drawFish(10, 10, 80, "black");
+drawFish(202, 200, 40, "orange");
+drawFish(15, 400, 100, "green");
+drawSolidBall(100, 300, "yellow", "1");
